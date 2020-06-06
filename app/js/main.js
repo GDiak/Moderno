45902a36cfd0036ada10c5c5a1500bad34137883
GDiak <ganna02ph@yahoo.com>
2020-06-06 16:42:39 -0700

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,4 @@
 $(function () {
-  var mixer = mixitup(".products__inner-box");
-
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
@@ -13,4 +11,34 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
   });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$",
+  });
+
+  $(".icon-th-list").on("click", function () {
+    $(".product__item").addClass("list");
+    $(".icon-th-list").addClass("active");
+    $(".icon-th").removeClass("active");
+  });
+  $(".icon-th").on("click", function () {
+    $(".product__item").removeClass("list");
+    $(".icon-th").addClass("active");
+    $(".icon-th-list").removeClass("active");
+  });
+
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").slideToggle();
+  });
+  $(".header__btn-menu").on("click", function () {
+    $(".header__box").toggleClass("active");
+  });
+
+  var mixer = mixitup(".products__inner-box");
+  //писать последним - после него ничего не работает
 });
